7c0a9c446697015efdb3e4b95737753e1c163a36
Steven Murray <steven.murray@curtin.edu.au>
2019-12-27 13:37:04 -0700

    Another test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -28,3 +28,18 @@ def test_get_parent(tmpdir: py.path.local):
     assert path.basename(parent) == "child"
     root = utils.get_parent_dir(direc.strpath, 2)
     assert path.basename(root) == "test_get_parent"
+
+
+def test_ymd_to_jd():
+    jd = utils.ymd_to_jd(2019, 1, 1)
+    assert jd == 1
+
+    jd = utils.ymd_to_jd(2019, 1, 30)
+    assert jd == 30
+
+    jd = utils.ymd_to_jd(2019, 3, 1)
+    assert jd == 60
+
+    # Ensure leap years go correctly
+    jd = utils.ymd_to_jd(2020, 3, 1)
+    assert jd == 61
