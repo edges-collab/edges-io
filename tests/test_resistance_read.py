ba59d8b2c999a30b05c0f1e465eac2bda49af9b2
Steven Murray <steven.g.murray@asu.edu>
2020-09-22 12:41:43 -0700

    test: read old and new restistance files

--- tests/test_resistance_read.py
+++ tests/test_resistance_read.py
@@ -1,19 +1,62 @@
+import pytest
+
 import shutil
 from pathlib import Path
 
 from edges_io.io import Resistance
 
 
-def test_resistance_read_header(datadir: Path, tmpdir: Path):
+def test_resistance_read_old_header(datadir: Path, tmpdir: Path):
 
-    header = Resistance.read_old_style_csv_header(datadir / "resistance_header.csv")
+    header, nlines = Resistance.read_old_style_csv_header(
+        datadir / "old_resistance_file.csv"
+    )
 
-    assert header["Start Time"] == "9/17/2017 10:25:01 AM"
+    assert header["Start Time"] == "9/14/2017 2:16:45 PM"
 
     shutil.copyfile(
-        datadir / "resistance_header.csv", tmpdir / "Ambient_1_2019_150_lab.csv"
+        datadir / "old_resistance_file.csv", tmpdir / "Ambient_1_2019_150_lab.csv"
     )
 
     path, _ = Resistance.check_self(tmpdir / "Ambient_1_2019_150_lab.csv", fix=True)
 
-    assert path.name == "Ambient_01_2017_260_10_25_01_lab.csv"
+    assert path.name == "Ambient_01_2017_257_14_16_45_lab.csv"
+
+
+def test_resistance_read_new(datadir: Path):
+    fl = (
+        datadir
+        / "Receiver01_25C_2019_11_26_040_to_200MHz/Resistance/Ambient_01_2019_329_16_02_35_lab.csv"
+    )
+
+    r = Resistance(fl)
+    r.read()
+    assert len(r.resistance) == 9
+    assert len(r.resistance.dtype.names) == 12
+    assert len(r.ancillary) == 0
+
+
+def test_resistance_read_old(datadir: Path):
+    fl = datadir / "old_resistance_file.csv"
+
+    r = Resistance(fl)
+    r.read()
+    assert len(r.resistance) == 12
+    assert len(r.resistance.dtype.names) == 11
+    assert len(r.ancillary) == 0
+
+
+def test_resistance_read_no_store(datadir: Path):
+    fl = (
+        datadir / "Receiver01_25C_2019_11_26_040_to_200MHz/Resistance"
+        "/Ambient_01_2019_329_16_02_35_lab.csv"
+    )
+
+    r = Resistance(fl, store_data=False)
+
+    # Should warn that we're not storing the data.
+    with pytest.warns(UserWarning):
+        r.resistance
+
+    with pytest.warns(UserWarning):
+        r.ancillary
