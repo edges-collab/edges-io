94537d76a41e1f3778512716672c8e3053baf024
Steven Murray <steven.g.murray@asu.edu>
2020-10-01 09:15:20 -0700

    fix: spectra run_num failed because spectra is a list

--- src/edges_io/io.py
+++ src/edges_io/io.py
@@ -613,7 +613,7 @@ class _SpectraOrResistanceFolder(_DataContainer):
     @property
     def run_num(self):
         """Dictionary of run numbers for each load"""
-        return {k: getattr(self, k).run_num for k in LOAD_ALIASES}
+        return {k: getattr(self, k)[0].run_num for k in LOAD_ALIASES}
 
     @classmethod
     def _check_all_files_there(cls, path: Path) -> bool:
