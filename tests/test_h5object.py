fe531432921dfcebb2db77d58a28e8cc88630519
Steven Murray <steven.g.murray@asu.edu>
2020-10-16 09:17:05 -0700

    test: add tests for extra keys in h5

--- tests/test_h5object.py
+++ tests/test_h5object.py
@@ -1,5 +1,7 @@
 import pytest
 
+import inspect
+import numpy as np
 from copy import deepcopy
 
 from edges_io.h5 import HDF5RawSpectrum, HDF5StructureExtraKey
@@ -8,9 +10,17 @@ from edges_io.h5 import HDF5RawSpectrum, HDF5StructureExtraKey
 def test_extra_key(fastspec_data):
     this = deepcopy(fastspec_data)
     this["meta"]["new_key"] = 2
+    this["spectra"]["new_spectrum"] = np.zeros(20)
 
     with pytest.warns(UserWarning):
-        HDF5RawSpectrum.from_data(this)
+        obj = HDF5RawSpectrum.from_data(this)
+
+    # Ensure we can still access 'new_spectrum' even though it's not in the _structure
+    assert len(obj["spectra"]["new_spectrum"]) == 20
+
+    # But we can't get stuff that doesn't exist at all.
+    with pytest.raises(KeyError):
+        obj["spectra"]["non-existent"]
 
     HDF5RawSpectrum._require_no_extra = True
 
@@ -54,5 +64,6 @@ def test_access_attrs(fastspec_spectrum_fl):
 
 def test_get_items(fastspec_spectrum_fl):
     obj = HDF5RawSpectrum(fastspec_spectrum_fl)
+    assert inspect.isgeneratorfunction(obj.items)
     for key, val in obj.items():
         assert key in obj.keys()
